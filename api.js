45318804f09c03042cc27c9d54f52a8aa3145465
cleidianeloy <cleidianeloy@hotmail.com>
2021-08-10 20:03:52 -0300

    primeiro commit

---
+++ api.js
@@ -0,0 +1,35 @@
+
+
+const xhr = new XMLHttpRequest();
+const url = "https://covid-api.mmediagroup.fr/v1/vaccines?country=Brazil";
+
+xhr.responseType = 'json';
+xhr.onreadystatechange = () =>{
+    console.log(xhr.readyState);
+
+    if(xhr.readyState === XMLHttpRequest.DONE){
+            resposta= xhr.response;
+            porcentagem(resposta);
+  }
+}
+
+xhr.open('GET',url);
+xhr.send();
+
+const porcentagem = (resposta) => {
+    populacao = resposta.All.population;
+    populacao_vacinada = resposta.All.people_vaccinated;
+    porcentagem_vacinados = (populacao_vacinada / populacao) * 100;
+    divResposta = document.querySelector("#resposta")
+    if(0.8 * populacao <= resposta.All.people_vaccinated){
+         console.log("pode lamber corrimão");
+         divResposta.innerHTML= "<h1 style='font-size: 3rem'>Pode lamber corrimão</h1> a porcentagem de vacinados contra o covid no Brasil é: "
+         + porcentagem_vacinados.toFixed(2) + "%";
+    }else{
+        console.log("não pode lamber corrimão (ainda) a porcentagem de vacinados hoje é: "
+        + porcentagem_vacinados.toFixed(2));
+        divResposta.innerHTML= "<h1 style='font-size: 3rem'> não pode lamber corrimão (ainda)</h1> a porcentagem de vacinados contra o covid no Brasil é: "
+        + porcentagem_vacinados.toFixed(2) + "%";
+        
+    }
+}

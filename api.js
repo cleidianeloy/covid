ac5b62d4cf3c32f4e0eaa3d459473a5a4dd6d9e6
cleidianeloy <cleidianeloy@hotmail.com>
2021-08-10 20:37:38 -0300

    alteração variaveis

--- api.js
+++ api.js
@@ -1,15 +1,12 @@
 
-
 const xhr = new XMLHttpRequest();
 const url = "https://covid-api.mmediagroup.fr/v1/vaccines?country=Brazil";
 
 xhr.responseType = 'json';
 xhr.onreadystatechange = () =>{
-    console.log(xhr.readyState);
-
     if(xhr.readyState === XMLHttpRequest.DONE){
-            resposta= xhr.response;
-            porcentagem(resposta);
+        resposta = xhr.response;
+        porcentagem(resposta);
   }
 }
 
@@ -21,10 +18,10 @@ const porcentagem = (resposta) => {
     populacao_vacinada = resposta.All.people_vaccinated;
     porcentagem_vacinados = (populacao_vacinada / populacao) * 100;
     divResposta = document.querySelector("#resposta")
-    if(0.8 * populacao <= resposta.All.people_vaccinated){
-         console.log("pode lamber corrimão");
-         divResposta.innerHTML= "<h1 style='font-size: 3rem'>Pode lamber corrimão</h1> a porcentagem de vacinados contra o covid no Brasil é: "
-         + porcentagem_vacinados.toFixed(2) + "%";
+    if(0.8 * populacao <= porcentagem_vacinados){
+        console.log("pode lamber corrimão");
+        divResposta.innerHTML= "<h1 style='font-size: 3rem'>Pode lamber corrimão</h1> a porcentagem de vacinados contra o covid no Brasil é: "
+        + porcentagem_vacinados.toFixed(2) + "%";
     }else{
         console.log("não pode lamber corrimão (ainda) a porcentagem de vacinados hoje é: "
         + porcentagem_vacinados.toFixed(2));
